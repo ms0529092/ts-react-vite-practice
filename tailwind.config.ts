80ffa5288b8926bdec792e7a0f1345682c42c2ac
Hans Lee <ms0529092@gmail.com>
2023-12-08 10:55:36 +0800

    添加preline 套件

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,21 @@
 import type { Config } from 'tailwindcss'
 
 export default {
+  darkMode:'class',
   content: [
     './index.html',
     './src/**/*.{js,jsx,ts,tsx}',
+    'node_modules/preline/dist/*.js',
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center:true
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('preline/plugin')
+  ],
 } satisfies Config
 
